3de1ed888a0bf32c2b5802b4e188b04edff849e5
Jamire44 <2003doyle@gmail.com>
2024-02-23 15:01:44 +0000

    JWT service added

--- src/main/java/com/iftekharSecurity/SecurityJWTPractice/service/UserDetailsImp.java
+++ src/main/java/com/iftekharSecurity/SecurityJWTPractice/service/UserDetailsImp.java
@@ -1,8 +1,28 @@
 package com.iftekharSecurity.SecurityJWTPractice.service;
 
+import com.iftekharSecurity.SecurityJWTPractice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
 import java.util.Collection;
 
-public class UserDetailsImp implements UserDetails {
-    
+@Service
+public class UserDetailsImp implements UserDetailsService {
 
+
+    private final UserRepository userRepository;
+
+    public UserDetailsImp(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
 }
